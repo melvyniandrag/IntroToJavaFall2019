47102d99dfeb08e3767d6badca993dddd46daaff
melvyn <melvyniandrag@gmail.com>
2019-08-22 12:49:52 -0400

    added alternative week 13 lecture

---
+++ Lectures/Lecture13-JavaAndImages/Readme.md
@@ -0,0 +1,11 @@
+# Images and processing.
+This lecture is important preamble for next week as well as your 
+
+## Standard Image formats
+Binary dump the images with java and look at wikipedia entries for the images. See that they are quite complex
+
+## PPM Image format
+Very easy - not really used anywhere that I've seen, but they are easy to understand. https://www.cs.swarthmore.edu/~soni/cs35/f12/Labs/extras/01/ppm_info.html
+
+## Exercise 
+Write a java program to create an NxN RGB PPM image with a red diagonal.
--- Lectures/Lecture14-ImageProcessingWithJava/SampleCode/ppmdecoder/_WITH_DEBUG_PPMDecoder.java
+++ Lectures/Lecture14-ImageProcessingWithJava/SampleCode/ppmdecoder/_WITH_DEBUG_PPMDecoder.java
@@ -1,3 +1,10 @@
+/*
+ * Going through this code I see a ton of obvious improvements that could be made. 
+ * Error handling, optimization, etc.. Don't get mad that they aren't there! 
+ * This is just demo code!
+ * For example - we don't verify that the ppm file is correct, i.e. contains valid data, correct dimensions, etc..
+ */
+
 package ppmdecoder;
 
 import java.io.*;
@@ -20,8 +27,16 @@ public class PPMDecoder{
 				System.out.println("Can't continue. Corrupt image format");
 			}
 			else{
+				System.out.println("$$$$$$$$$$$$$ Binary Input:   $$$$$$$$$$$$$");
+				ppmdecoder.binaryDump();
+				System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
 				String message = ppmdecoder.decode();
+				System.out.println("$$$$$$$$$$$$$ Extracted Msg   $$$$$$$$$$$$$");
+				ppmdecoder.binaryDumpExtractedBits();
+				System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+				System.out.println("############# Decoded String: #############");
 				System.out.println(message);
+				System.out.println("###########################################");
 			}
 		} catch ( Exception e ){
 			System.out.println(e.toString());
@@ -61,6 +76,13 @@ public class PPMDecoder{
 		}
 	}
 
+	/*
+	 * Assume that the last 2 bytes of every channel are used for an encrypted message.
+	 * We have 6 pixels, with 3 channels each so that give 6 * 3 * 2 bits we can use for our messsage.
+	 * That is only 36, so the message will be small!
+	 * Assume that the message is ASCII encoded. That means that each character is 8 bits long, so we can fit 
+	 * four characters in the message.:wq
+	 */
 	public String decode(){
 		for( int pixel = 0; pixel < _pixelValues.size(); ++pixel){
 			for( int channel = 0; channel < 3; ++channel ){
@@ -88,8 +110,28 @@ public class PPMDecoder{
 		return new String(bytes, UTF8_CHARSET);
 	}
 
+	private void binaryDump(){
+		for( int pixel = 0; pixel < _pixelValues.size(); ++pixel ){
+			String s = String.format("%8s\t%8s\t%8s",
+						Integer.toBinaryString(_pixelValues.get(pixel).get(0) & 0xFF),
+						Integer.toBinaryString(_pixelValues.get(pixel).get(1) & 0xFF),
+						Integer.toBinaryString(_pixelValues.get(pixel).get(2) & 0xFF)
+					).replace(' ', '0');
+			System.out.println(s);
+		}
+
+	}
+
+	private void binaryDumpExtractedBits(){
+		for( int i = 0; i < encodedBytes.size(); ++i){
+			String s = String.format("%8s", Integer.toBinaryString(encodedBytes.get(i) & 0xFF));
+			System.out.println(s);
+		}
+	}
+
 	private ArrayList<Integer> getDimensions(String line){
 		String[] split = line.split(" "); // assumes only spaces. We could improve by splitting on white space in case of a tab.
+		//also we assume there will be two numbers. No error handling in this code for simplicity!
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		ret.add(Integer.valueOf(split[0]));
 		ret.add(Integer.valueOf(split[1]));
